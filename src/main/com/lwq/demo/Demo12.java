fee9f09ff668dab8f6d00c6630f23aa8247f13c4
Lwq <943904454@qq.com>
2018-09-17 10:51:39 +0800

    之字形打印增加注释

--- src/main/com/lwq/demo/Demo12.java
+++ src/main/com/lwq/demo/Demo12.java
@@ -36,12 +36,12 @@ public class Demo12 {
         int dC = 0;
         int endR = matrix.length - 1;
         int endC = matrix[0].length - 1;
-        boolean fromUp = true;
+        boolean fromUp = false;
         while (tR != endR + 1) {
             printLevel(matrix, tR, tC, dR, dC, fromUp);
-            tR = tC == endC ? tR + 1 : tR;
+            tR = tC == endC ? tR + 1 : tR;  //t点每次向右移动，移动到最后一列，然后在向下移动
             tC = tC == endC ? tC : tC + 1;
-            dC = dR == endR ? dC + 1 : dC;
+            dC = dR == endR ? dC + 1 : dC;  //d点每次向下移动，移动到最后一行，再向右移动
             dR = dR == endR ? dR : dR + 1;
             fromUp=!fromUp;//去掉这一行就是斜线打印
         }
@@ -51,10 +51,12 @@ public class Demo12 {
     public static void printLevel(int[][] m, int tR, int tC, int dR, int dC,
                                   boolean f) {
         if (f) {
+            //往下
             while (tR != dR + 1) {
                 System.out.print(m[tR++][tC--] + " ");
             }
         } else {
+            //往上
             while (dR != tR - 1) {
                 System.out.print(m[dR--][dC++] + " ");
             }
